32f93e2ab24a3198ab241b2c006ba15e4cc91fb7
samifaith <60042337+samifaith@users.noreply.github.com>
2020-03-27 23:47:38 -0400

    demoday update

--- public/main.js
+++ public/main.js
@@ -1,43 +1,71 @@
-var update = document.getElementById('update')
-var del = document.getElementById('delete')
+var thumbUp = document.getElementsByClassName("fa-thumbs-up");
+var thumbDown = document.getElementsByClassName("fa-thumbs-down");
+let remove = document.getElementsByClassName("fa-times-circle");
 
+Array.from(remove).forEach(function(element) {
+      element.addEventListener('click', function(){
+        const genre = this.parentNode.parentNode.childNodes[1].innerText
+        fetch('messages', {
+          method: 'delete',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            'genre': genre
+          })
+        }).then(function (response) {
+          window.location.reload()
+        })
+      });
+});
 
-//UPDATE BUTTON  fetch request
-update.addEventListener('click', function () {
-  fetch('quotes', {
-  method: 'put',
-  headers: {'Content-Type': 'application/json'},
-  body: JSON.stringify({
-    'name': 'Darth Vader',
-    'quote': 'I find your lack of faith disturbing.'
-  })
-})
-.then(res => {
-  if (res.ok) return res.json()
-}).
-then(data => {
-  console.log(data)
-  window.location.reload()
-})
-})
-
-
-// DELETE BUTTON fetch request
-del.addEventListener('click', function () {
-  fetch('quotes', {
-    method: 'delete',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      'name': 'Darth Vader'
-    })
-  })
-  .then(res => {
-    if (res.ok) return res.json()
-  }).
-  then(data => {
-    console.log(data)
-    window.location.reload()
-  })
-})
+// Array.from(thumbUp).forEach(function(element) {
+//       element.addEventListener('click', function(){
+//         const name = this.parentNode.parentNode.childNodes[1].innerText
+//         const msg = this.parentNode.parentNode.childNodes[3].innerText
+//         const thumbUp = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
+//         console.log(thumbUp)
+//         fetch('messages', {
+//           method: 'put',
+//           headers: {'Content-Type': 'application/json'},
+//           body: JSON.stringify({
+//             'name': name,
+//             'msg': msg,
+//             'thumbUp':thumbUp
+//           })
+//         })
+//         .then(response => {
+//           if (response.ok) return response.json()
+//           console.log(response)
+//         })
+//         .then(data => {
+//           console.log(data)
+//           window.location.reload(true)
+//         })
+//       });
+// });
+//
+// Array.from(thumbDown).forEach(function(element) {
+//       element.addEventListener('click', function(){
+//         const name = this.parentNode.parentNode.childNodes[1].innerText
+//         const msg = this.parentNode.parentNode.childNodes[3].innerText
+//         const thumbUp = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
+//         console.log(thumbUp)
+//         fetch('messagesDown', {
+//           method: 'put',
+//           headers: {'Content-Type': 'application/json'},
+//           body: JSON.stringify({
+//             'name': name,
+//             'msg': msg,
+//             'thumbUp':thumbUp
+//           })
+//         })
+//         .then(response => {
+//           if (response.ok) return response.json()
+//         })
+//         .then(data => {
+//           console.log(data)
+//           window.location.reload(true)
+//         })
+//       });
+// });
--- server.js
+++ server.js
@@ -1,58 +1,67 @@
-const express = require('express');
-const app = express();
+// server.js
+
+// set up ======================================================================
+// get all the tools we need
+var express  = require('express');
+var app      = express();
+var port     = process.env.PORT || 8080;
 const MongoClient = require('mongodb').MongoClient
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended: true}))
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash    = require('connect-flash');
+
+var morgan       = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser   = require('body-parser');
+var session      = require('express-session');
+
+var configDB = require('./config/database.js');
+
 var db
-app.use(bodyParser.json())
+
+// configuration ===============================================================
+mongoose.connect(configDB.url, (err, database) => {
+  if (err) return console.log(err)
+  db = database
+  require('./app/routes.js')(app, passport, db);
+}); // connect to our database
+
+//app.listen(port, () => {
+    // MongoClient.connect(configDB.url, { useNewUrlParser: true }, (error, client) => {
+    //     if(error) {
+    //         throw error;
+    //     }
+    //     db = client.db(configDB.dbName);
+    //     console.log("Connected to `" + configDB.dbName + "`!");
+    //     require('./app/routes.js')(app, passport, db);
+    // });
+//});
+
+require('./config/passport')(passport); // pass passport for configuration
+
+// set up our express application
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser.json()); // get information from html forms
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'))
 
+app.set('view engine', 'ejs'); // set up ejs for templating
 
-app.get('/', (req, res) => {
-  db.collection('quotes').find().toArray((err, result) => {
-    if (err) return console.log(err)
-    res.render('index.ejs', {quotes: result})
-  })
-})
+// required for passport
+app.use(session({
+    secret: 'bookit', // session secret
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
 
-MongoClient.connect('mongodb+srv://demo:demo@cluster0-eyipj.mongodb.net/test?retryWrites=true&w=majority', (err, client) => {
-  if (err) return console.log(err)
-  db = client.db('star-wars-quotes')
-  app.listen(3000, () => {
-    console.log('listening on 3000')
-  })
-})
 
-app.post('/quotes', (req, res) => {
-  db.collection('quotes').insertOne(req.body, (err, result) => {
-    if (err) return console.log(err)
+// routes ======================================================================
+//require('./app/routes.js')(app, passport, db); // load our routes and pass in our app and fully configured passport
 
-    console.log('saved to database')
-    res.redirect('/')
-  })
-})
-
-app.set('view engine', 'ejs')
-app.put('/quotes', (req, res) => {
-  db.collection('quotes')
-  .findOneAndUpdate({name: 'Yoda'}, {
-    $set: {
-      name: req.body.name,
-      quote: req.body.quote
-    }
-  }, {
-    sort: {_id: -1},
-    upsert: true
-  }, (err, result) => {
-    if (err) return res.send(err)
-    res.send(result)
-  })
-})
-
-app.delete('/quotes', (req, res) => {
-  db.collection('quotes').findOneAndDelete({name: req.body.name},
-  (err, result) => {
-    if (err) return res.send(500, err)
-    res.send({message: 'A darth vadar quote got deleted'})
-  })
-})
+// launch ======================================================================
+app.listen(port);
+console.log('The magic happens on port ' + port);
