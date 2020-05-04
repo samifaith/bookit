db904def0a26c0c75574cc6ae8530f3a71abaa83
samifaith <60042337+samifaith@users.noreply.github.com>
2020-05-03 22:46:27 -0400

    update

--- public/fave.js
+++ public/fave.js
@@ -2,12 +2,7 @@ let trash = document.getElementsByClassName("fa fa-times-circle")
 
 Array.from(trash).forEach(function(element) {
   element.addEventListener('click', function(){
-    // const bookImg = this.parentNode.parentNode.childNodes[1].src
-    // const bookTitle = this.parentNode.parentNode.childNodes[3].innerHTML
-    // const bookAuthor = this.parentNode.parentNode.childNodes[5].innerHTML
     const postId = this.parentNode.parentNode.childNodes[7].innerHTML
-    // console.log(this.parentNode.parentNode.childNodes[7].innerHTML)
-    // console.log(bookTitle, bookAuthor, 'test')
     fetch('faveDelete', {
       method: 'delete',
       headers: {
